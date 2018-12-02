d0fd4026fb153745f8b0cd4e6bcf0fb465d25b55
bjorngronbaek <bjgr@DESKTOP-88U54DG.localdomain>
2018-12-02 21:14:15 +0100

    added 01_2 solution

--- 01_1/01_2.rs
+++ 01_1/01_2.rs
@@ -3,42 +3,44 @@ use std::io;
 use std::io::prelude::*;
 use std::env;
 use std::collections::HashSet;
+use std::vec::Vec;
 
-fn read_all_lines(filename: &str) -> io::Result<()> {    
+fn read_all_lines(filename: &str) -> io::Result<()> {
     let file = File::open(&filename)?;
     let reader = io::BufReader::new(file);
 
+    let mut changes = Vec::new();
+    for line in reader.lines() {
+        let line = line?;
+        let value: i32 = line.parse().unwrap();
+        changes.push(value);
+    }
+
+    let mut frequencies: HashSet<i32> = HashSet::new();
     let mut sum = 0;
     let mut dup_freq = 0;
     let mut found_freq = false;
-    for line in reader.lines() {
-        let line = line?;
 
-        let value: i32 = line.parse().unwrap();
-        sum = sum + value;
+    while !found_freq {
+        for change in &changes {
+            sum = sum + change;
 
-        println!("{}\t{}", line,sum);
-
-        if !found_freq && frequencies.contains(&sum) {
-            println!("First duplicate freq: {}",sum);            
-            dup_freq = sum;
-            found_freq = true;
+            if !found_freq && frequencies.contains(&sum) {
+                dup_freq = sum;
+                found_freq = true;
+            }
+            frequencies.insert(sum);
         }
-        frequencies.insert(sum);
     }
 
-    println!("Sum: {}",sum);
     if found_freq {
         println!("First duplicate freq: {}",dup_freq);
     }
-    else {
-        read_all_lines(&filename,&frequencies);
-    }
+    
     Ok(())
 }
 
 fn main () {
     let args: Vec<String> = env::args().collect();
-    let mut frequencies = HashSet<>::new();
-    let _result = read_all_lines(&args[1],frequencies);    
+    let _result = read_all_lines(&args[1]);    
 }
