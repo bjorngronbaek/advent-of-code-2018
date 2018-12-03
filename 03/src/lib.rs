4e057c2309beb2e5a69bc6597aec345aae4a243d
Bjørn Grønbæk <git@gronbaek.net>
2018-12-03 23:32:36 +0100

    Solved 3_1. Added check for 3_2

---
+++ 03/src/lib.rs
@@ -0,0 +1,90 @@
+use std::fmt;
+
+pub struct Claim {
+    pub id: u32,
+    pub x: u32,
+    pub y: u32,
+    pub length: u32,
+    pub height: u32
+}
+
+impl Claim{
+    //#1 @ 1,3: 4x4
+    pub fn new(id: u32, x:u32, y:u32, length:u32, height: u32) -> Claim {
+        Claim {
+            id:id,
+            x:x,
+            y:y,
+            length:length,
+            height:height
+        }
+    }
+
+    pub fn overlaps(self, claim:Claim) -> bool {
+        let tl_1 = (self.x,self.y);
+        let br_1 = (self.x+self.length,self.y+self.height);
+
+        let tl_2 = (claim.x,claim.y);
+        let br_2 = (claim.x+claim,.length,claim.y+claim.height);
+
+        if tl_1.0 > br_2.0 || tl_2.0 > br.0 {
+            false
+        }
+
+        if tl_1.1 < br_2.1 || tl_2.1 > br_2.1 {
+            false
+        }
+
+        true
+    }
+}
+
+pub struct Piece {
+    width: u32,
+    height: u32,
+    pub claim_count: Vec<u32>
+}
+
+impl Piece {
+    pub fn new(width :u32, height :u32) -> Piece {
+        let claims = (0..width * height).map(|_x| 0).collect();
+        Piece {
+            width: width,
+            height: height,
+            claim_count: claims
+        }
+    }
+
+    fn get_index(&self, row: u32, column: u32) -> usize {
+        (row * self.width + column) as usize
+    }
+
+    pub fn render(&self) -> String {
+        self.to_string()
+    }
+
+    pub fn claim(&mut self, claim: &Claim) {
+        for dy in 0..claim.height {
+            for dx in 0..claim.length {
+                let xpos = claim.x + dx;
+                let ypos = claim.y + dy;
+                //println!("Updating {}x{}",xpos,ypos);
+                let index = self.get_index(ypos,xpos);
+                self.claim_count[index] = self.claim_count[index] + 1;
+            }
+        }
+    }
+}
+
+impl fmt::Display for Piece {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        for line in self.claim_count.as_slice().chunks(self.width as usize) {
+            for &claim_count in line {
+                write!(f, "{}", claim_count)?;
+            }
+            write!(f, "\n")?;
+        }
+
+        Ok(())
+    }
+}
--- 03/src/main.rs
+++ 03/src/main.rs
@@ -1,3 +1,58 @@
+extern crate aoc03;
+extern crate regex;
+
+use aoc03::Claim;
+use aoc03::Piece;
+use regex::Regex;
+
+use std::fs::File;
+use std::io;
+use std::io::prelude::*;
+use std::env;
+use std::vec::Vec;
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+    let claims : Vec<Claim> = read_all_lines(&args[1]).unwrap();
+
+    let mut piece = Piece::new(1000,1000);
+    for claim in claims {
+        piece.claim(&claim);
+    }
+
+    //println!("{}",piece.render());
+
+    let mut count = 0;
+    for claim in piece.claim_count {
+        if claim >= 2 {
+            count += 1;
+        }
+    }
+
+    println!("{} inches claimed by two or more elves",count);
 }
+
+fn read_all_lines(filename: &str) -> io::Result<Vec<Claim>> {
+    let file = File::open(&filename)?;
+    let reader = io::BufReader::new(file);
+
+    //#1 @ 257,829: 10x23
+    let re = Regex::new(r"#(\d+) @ (\d+),(\d+): (\d+)x(\d+)").unwrap();
+    let mut claims: Vec<Claim> = vec![];
+
+    for line in reader.lines() {
+        let line = line?;
+        //println!("{}",&line);
+        for capture in re.captures_iter(&line) {
+            let id :u32 = capture[1].parse().unwrap();
+            let x :u32  = capture[2].parse().unwrap();
+            let y :u32  = capture[3].parse().unwrap();
+            let lenght :u32  = capture[4].parse().unwrap();
+            let height :u32  = capture[5].parse().unwrap();
+            claims.push(Claim::new(id,x,y,lenght,height));
+        }
+    }
+
+    Ok(claims)
+}
+
