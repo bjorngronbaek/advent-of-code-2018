6a0851500de2e9e260f39fb38ecf6fb53970645f
Bjørn Grønbæk <git@gronbaek.net>
2018-12-06 00:44:22 +0100

    solved 05_2

--- 05/java/main.java
+++ 05/java/main.java
@@ -1,5 +1,6 @@
 import java.nio.file.*;
 import java.io.*;
+import java.util.*;
 
 public class main{
 
@@ -17,24 +18,52 @@ public class main{
         }
 
         String s = sb.toString();    
-        search(s);
+        /* 05-1
+        main searcher = new main(s);
+        searcher.search(s);
+        */
+
+        HashSet<String> units = new HashSet<String>();
+        for (char ch: s.toCharArray()) {
+            units.add(Character.toString(ch).toLowerCase());
+        }
+
+        int unitcount = s.length();
+        for (String temp : units) {
+            String s1 = s.replace(temp,"");
+            String s2 = s1.replace(temp.toUpperCase(),"");
+            System.out.println("Searching "+temp);
+            main searcher = new main(s2);
+            int c = searcher.search(s2);
+            if(c < unitcount){
+                System.out.println("updating");
+                unitcount = c;
+            }            
+        }
+        System.out.println("Min count "+unitcount);
     }
 
-    public static void search(String s){
+    private String s;
+    public main(String s){
+        this.s = s;
+    }
+
+    public int search(String s){
         System.out.println("Searching "+s);
         String pair = findPair(s);
         if(pair != null){
             System.out.println("Found "+pair);
             s = s.replace(pair,"");
-            search(s);
+            return search(s);
         }
         else{
-            System.out.println(s);
+            //System.out.println(s);
             System.out.println("Found "+s.length()+" units");
         }
+        return s.length();
     }
 
-    public static String findPair(String s){
+    public String findPair(String s){
         for(int i=0; i<s.length()-1;i++){
             String first = Character.toString(s.charAt(i));
             String second = Character.toString(s.charAt(i+1));
