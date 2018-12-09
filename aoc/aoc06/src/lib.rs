cd6c7f267eed54b9714add052c201db6487f3217
Bjørn Grønbæk <git@gronbaek.net>
2018-12-09 20:23:59 +0100

    aoc_06

--- aoc/aoc06/src/lib.rs
+++ aoc/aoc06/src/lib.rs
@@ -1,20 +1,22 @@
 extern crate regex;
 use regex::Regex;
+use std::collections::HashSet;
 
 #[cfg(test)]
 mod tests {
 
     use std::collections::hash_map::DefaultHasher;
     use std::hash::{Hash, Hasher};
+    use std::collections::HashSet;
 
     #[test]
     fn test_point_m_distance() {
-        let &p1 = ::Point::new(0,0);
+        let p1 = ::Point::new(0,0);
         assert_eq!(0,p1.m_distance(&::Point::new(0,0)));
         assert_eq!(2,p1.m_distance(&::Point::new(1,1)));
         assert_eq!(4,p1.m_distance(&::Point::new(2,2)));
 
-        let &p2 = ::Point::new(-1,-1);
+        let p2 = ::Point::new(-1,-1);
         assert_eq!(4,p2.m_distance(&::Point::new(1,1)));
         assert_eq!(0,p2.m_distance(&::Point::new(-1,-1)));
         assert_eq!(2,p2.m_distance(&::Point::new(-2,-2)));
@@ -37,11 +39,60 @@ mod tests {
         t.hash(&mut s);
         s.finish()
     }
+
+    #[test]
+    fn test_closest(){
+        let target = ::Point::new(0,0);
+        let p1 = ::Point::new(1,1);
+        let p2 = ::Point::new(2,2);
+        let mut points: HashSet<::Point> = HashSet::new();
+        points.insert(p1);
+        points.insert(p2);
+
+        assert_eq!(::Point::new(1,1), target.find_closest_point(&points).unwrap());
+        assert_eq!(::Point::new(1,1), ::Point::new(1,1).find_closest_point(&points).unwrap());
+        assert_eq!(::Point::new(2,2), ::Point::new(3,3).find_closest_point(&points).unwrap());
+        assert_ne!(::Point::new(1,1), ::Point::new(3,3).find_closest_point(&points).unwrap());
+
+        assert_eq!(::Point::new(2,1).find_closest_point(&points).is_none(),true);
+    }
+
+    #[test]
+    fn test_totaldistance(){
+        let target = ::Point::new(0,0);
+        let p1 = ::Point::new(1,1);
+        let p2 = ::Point::new(2,2);
+        let mut points: HashSet<::Point> = HashSet::new();
+        points.insert(p1);
+        points.insert(p2);
+
+        assert_eq!(6, target.find_total_distance(&points));
+    }
+
+    #[test]
+    fn test_closest_specific(){
+        let target = ::Point::new(4,1);
+        let p1 = ::Point::new(1,1);
+        let p2 = ::Point::new(3,4);
+        let mut points: HashSet<::Point> = HashSet::new();
+        points.insert(p1);
+        points.insert(p2);
+
+        assert_eq!(::Point::new(1,1), target.find_closest_point(&points).unwrap());
+    }
+
+    #[test]
+    fn test_distance_specific(){
+        let target = ::Point::new(4,1);
+        let p1 = ::Point::new(1,1);
+        let p2 = ::Point::new(3,4);
+
+        assert_eq!(3,p1.m_distance(&target));
+        assert_eq!(4,p2.m_distance(&target));
+    }
 }
 
-#[derive(Debug)]
-#[derive(Eq)]
-#[derive(Hash)]
+#[derive(Hash, Eq, PartialEq, Debug)]
 pub struct Point {
     pub x: i32,
     pub y: i32,
@@ -69,7 +120,7 @@ impl Point {
         ((self.x - p.x).abs() + (self.y - p.y).abs()) as usize
     }
 
-    pub fn find_borders(points: &Vec<Point>) -> (Point,Point) {
+    pub fn find_borders(points: &HashSet<Point>) -> (Point,Point) {
         let mut extreem_left = std::i32::MAX;
         let mut extreem_right = std::i32::MIN;
         let mut extreem_bottom = std::i32::MIN;
@@ -95,23 +146,37 @@ impl Point {
 
         (Point::new(extreem_left,extreem_top),Point::new(extreem_right,extreem_bottom))
     }
-}
 
-impl PartialEq for Point {
-    fn eq(&self, other: &Point) -> bool {
-        self.x == other.x && self.y == other.y
+    pub fn find_closest_point(&self, points: &HashSet<Point>) -> Option<Point> {
+        let mut closest_dist = std::usize::MAX;
+        let mut closest_point = Point::new(0,0);
+        let mut closest_point_count = 0;
+        for point in points {
+            let dist = point.m_distance(self);
+            if dist == closest_dist {
+                closest_point_count += 1;
+            }
+            if dist < closest_dist {
+                closest_point_count = 1;
+                closest_dist = dist;
+                closest_point = Point::new(point.x,point.y);
+            }        
+        }
+
+        if closest_point_count == 1 {
+            Some(closest_point)
+        }
+        else{
+            None
+        }
+    }
+
+    pub fn find_total_distance(&self, points: &HashSet<Point>) -> usize {
+        let mut total_distance = 0;
+        for point in points {
+            total_distance += point.m_distance(self);
+        }
+
+        total_distance
     }
 }
-
-/*
-impl Hash for Point {
-    fn hash<H>(&self, state: &mut H)
-    where
-        H: Hasher,
-    {
-        state.write_u32(self.x);
-        state.write_u32(self.y);
-        state.finish();
-    }
-}
-*/
--- aoc/aoc06/src/main.rs
+++ aoc/aoc06/src/main.rs
@@ -3,6 +3,7 @@ extern crate aoc06;
 use std::env;
 use std::process;
 use std::collections::HashMap;
+use std::collections::HashSet;
 
 use aoc_helper::Config;
 use aoc06::Point;
@@ -15,30 +16,67 @@ fn main() {
         process::exit(1);
     });
 
-    let mut points: Vec<Point> = Vec::new();
     let entries = aoc_helper::run(config).unwrap();
+    let mut owner_map: HashMap<Point,usize> = HashMap::new();
+    let mut owner_set: HashSet<Point> = HashSet::new();
     for entry in entries {
-        points.push(Point::parse(&entry));
+        owner_map.insert(Point::parse(&entry),0);
+        owner_set.insert(Point::parse(&entry));
     }
 
-    for point in &points {
-        println!("Point: ({},{})",point.x,point.y);
-    }
+    let (tl,br) = Point::find_borders(&owner_set);
+    println!("Borders are: {:?} - {:?}",tl,br);
 
-    let (tl,br) = Point::find_borders(&points);
-    println!("{},{} {},{}",tl.x,tl.y,br.x,br.y);
+    let mut border_points : HashSet<Point> = HashSet::new();
 
-    let mut owner_map: HashMap<Point,Point> = HashMap::new;
-    for i in tl.x+1..br.x-1 {
-        for j in tl.y+1..br.y-1 {
-            let mut closest_dist = std::usize::MAX;
-            for point in &points{
-                let dist = point.m_distance(new::Point(i,j));
-                if dist < closest_dist {
-                    closest_dist = dist;
-                    let owner = owner_map.insert(Point::new(i,j),p);
-                }
-            }            
+    //calculate ownership of all coordinates within border and update the count
+    let mut distance_map: HashMap<Point,usize> = HashMap::new();
+    for i in tl.x..br.x+1 {
+        for j in tl.y..br.y+1 {
+            let target = Point::new(i,j);
+            let point = target.find_closest_point(&owner_set);
+            let distance = target.find_total_distance(&owner_set);
+            distance_map.insert(target,distance);
+
+            match point {
+                Some(p) => {
+                    if i == tl.x || i == br.x || j == tl.y || j == br.y {
+                        //println!("Found closes to border at ({},{}):\t{:?}",i,j,p);
+                        border_points.insert(p);
+                    }
+                    else {
+                        *owner_map.get_mut(&p).unwrap() += 1;
+                    }
+                },
+                None => {}
+            }
         }
     }
+
+    //if the point owns a border coordinate, remove it!
+    for p in &border_points {
+        //println!("{:?} has ownership on the border",p);
+        owner_map.remove(p);
+    }
+
+    let mut max_area = std::usize::MIN;
+    let mut max_owner = &Point::new(0,0);
+    for (point,count) in &owner_map {
+        //println!("({:?}); \t{}",point,count);
+        if *count > max_area {
+            max_owner = &point;
+            max_area = *count;
+        }
+    }
+    println!("Max point is: {:?} with {}",max_owner,max_area);
+
+    let mut size = 0;
+    for (point,distance) in &distance_map {        
+        if *distance < 10000 {
+            size += 1;
+        }
+    }
+    println!("Size of region: {}",size);
+
+
 }
